7da46c5f61b7c0ec556398401b7587f174986601
amd12 <55095558+amd12@users.noreply.github.com>
2020-06-19 16:47:46 +0300

    Update main.cpp

--- Test4/src/main.cpp
+++ Test4/src/main.cpp
@@ -42,14 +42,14 @@ unsigned long lastTame;
 /************************* WiFi Access Point *********************************/
 
 #define WLAN_SSID       "ASUS_MI"
-#define WLAN_PASS       "amd124672"
+#define WLAN_PASS       ""
 
 /************************* Adafruit.io Setup *********************************/
 
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "AMD12"
-#define AIO_KEY         "aio_UHwC48AOrT6WueGvAVHhaoUplRvp"
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
 
 /************ Global State (you don't need to change this!) ******************/
 
